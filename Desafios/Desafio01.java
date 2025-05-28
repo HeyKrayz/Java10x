1ec68654ec7ccaa031ac52f57c2851556b47db2d
Denis <denissilvestre007@gmail.com>
2025-05-28 03:28:10 -0300

    Add files via upload

---
+++ Desafios/Desafio01.java
@@ -0,0 +1,34 @@
+package Desafios;
+
+public class Desafio01 {
+    public static void main(String[] args) {
+//   perfil do naruto
+        String Nick = "Naruto";
+        int Idade = 16;
+        String Missão = "ir até outra aldeia";
+        char NivelDeDificuldade = 'B';
+        boolean StatusDeConclusão = true;
+
+//    perfil do sauke
+        String Nick2 = "Sasuke";
+        int Idade2 = 16;
+        String Missão2 = "Derrotar 3 Chunnins";
+        char NivelDeDificuldade2 = 'A';
+        boolean StatusDeConclusão2 = true;
+
+//    perfil do konohamaru
+        String Nick3 = "konohamaru";
+        int Idade3 = 12;
+        String Missão3 = "resgatar um gato perdido";
+        char NivelDeDificuldade3 = 'D';
+        boolean StatusDeConclusão3 = false;
+
+        System.out.println("Ninja: " + Nick + "\n Idade: " + Idade + "\n Sua missão: " + Missão + "\n Missão de nivel: " + NivelDeDificuldade + "\n Foi concluida: " + StatusDeConclusão);
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Ninja: " + Nick2 + "\n Idade: " + Idade2 + "\n Sua missão: " + Missão2 + "\n Missão de nivel: " + NivelDeDificuldade2 + "\n Foi concluida: " + StatusDeConclusão2);
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Ninja: " + Nick3 + "\n Idade: " + Idade3 + "\n Sua missão: " + Missão3 + "\n Missão de nivel: " + NivelDeDificuldade3 + "\n Foi concluida: " + StatusDeConclusão3);
+
+
+    }
+}
---
+++ Desafios/Desafio02.java
@@ -0,0 +1,56 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class Desafio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int NinjaCadastrado = 0;
+        int NumeroMax = 10;
+        String[] Ninja = new String[NumeroMax];
+        int opcao = 0;
+        while (true) {
+            System.out.println("\n==== Menu Ninja ====");
+            System.out.println("1 - Cadastrar Ninja");
+            System.out.println("2 - Listar Ninjas");
+            System.out.println("3 - Fechar Programa");
+            System.out.print("Escolha: ");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    if (NinjaCadastrado < NumeroMax) {
+                        System.out.print("Digite o nome do seu ninja: ");
+                        String nomeNinja = sc.nextLine();
+                        Ninja[NinjaCadastrado] = nomeNinja;
+                        NinjaCadastrado++;
+                    } else {
+                        System.out.println("A lista de ninjas está cheia.");
+                    }
+                    break;
+
+                case 2:
+                    if (NinjaCadastrado == 0) {
+                        System.out.println("Nenhum ninja cadastrado ainda.");
+                    } else {
+                        System.out.println("===== Lista de Ninjas =====");
+                        for (int i = 0; i < NinjaCadastrado; i++) {
+                            System.out.println(Ninja[i]);
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Fechando o programa...");
+                    sc.close();
+                    break;
+
+                default:
+                    System.out.println("Essa opção não é válida.");
+                    break;
+            }
+        }
+    }
+}
