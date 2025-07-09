63fe13655b91c6a9dc8455e90f9a707dd2ec3f27
Denis <denissilvestre007@gmail.com>
2025-07-08 21:23:54 -0300

    Add files via upload

---
+++ MiddleLevel/Construtores/Hokages.java
@@ -0,0 +1,13 @@
+package MiddleLevel.Construtores;
+
+public class Hokages {
+    String nome;
+    int idade;
+    boolean vivoOuMorto;
+
+    public Hokages (String nome, int idade, boolean vivoOuMorto) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuMorto = vivoOuMorto;
+    }
+}
---
+++ MiddleLevel/Construtores/Main.java
@@ -0,0 +1,10 @@
+package MiddleLevel.Construtores;
+
+public class Main {
+    public static void main(String[] args) {
+        Hokages Hashirama = new Hokages("Hashirama senju", 44,false);
+        Hokages Tobirama = new Hokages("Tobirama Senju",39,false);
+        Hokages Hiruzen = new Hokages("Hiruzen Sarutobi", 60,false);
+        Hokages Minato = new Hokages("Minato namikaze",33, false);
+    }
+}
